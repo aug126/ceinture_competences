973e9408d9ff0f9b759f575f4b96a1229d099954
aug126 <doat.augustin@gmail.com>
2019-08-28 22:24:24 +0200

    seeders complet de 3 classes en cours

---
+++ database/factories/UpdateFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Update;
+use Faker\Generator as Faker;
+
+$factory->define(Update::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
--- database/seeds/ClassesTableSeeder.php
+++ database/seeds/ClassesTableSeeder.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Color;
+use App\Program;
+use App\Skill;
+use App\User;
 use Illuminate\Database\Seeder;
 
 class ClassesTableSeeder extends Seeder
@@ -11,10 +15,26 @@ class ClassesTableSeeder extends Seeder
      */
     public function run()
     {
-        $user = factory(App\User::class)->create();
+        /* USER
+        -------------------------- */
+        // $user = factory(App\User::class)->create();
+        User::insert([
+            'name' => 'augustin',
+            'email' => 'doat.augustin@gmail.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('admin'), // password
+            'remember_token' => Str::random(10),
+        ]);
+        $user = User::first();
+
+        /* CLASSES
+        -------------------------- */
         factory(App\Classe::class, 3)
             ->create(['user_id' => $user->id])
             ->each(function ($classe) {
+
+                /* STUDENTS
+                -------------------------- */
                 $a = 1;
                 while ($a <= 25) {
                     $student = factory(App\Student::class)->create([
@@ -25,6 +45,79 @@ class ClassesTableSeeder extends Seeder
                         ->save($student);
                     $a++;
                 }
+
+                /* PROGRAMS
+                -------------------------- */
+                Program::insert([
+                    'program_name'  => 'Programme par défaut',
+                    'classe_id'     => $classe->id,
+                    'created_at'    => now()
+                ]);
+                $program = Program::latest('created_at')->first();
+
+                /* SKILLS
+                -------------------------- */
+                Skill::insert([
+                    [
+                        'skill_name' => 'Grammaire',
+                        'program_id' => $program->id
+                    ],[
+                        'skill_name' => 'Conjugaison',
+                        'program_id' => $program->id
+                    ],[
+                        'skill_name' => 'Orthographe',
+                        'program_id' => $program->id
+                    ],[
+                        'skill_name' => 'Numération',
+                        'program_id' => $program->id
+                    ],[
+                        'skill_name' => 'Calculs Écrits',
+                        'program_id' => $program->id
+                    ],[
+                        'skill_name' => 'Mesures',
+                        'program_id' => $program->id
+                    ],[
+                        'skill_name' => 'Géométrie',
+                        'program_id' => $program->id
+                    ],
+                ]);
+                $skills = $program->skills()->get();
+
+                /* COLORS
+                -------------------------- */
+                foreach($skills as $i => $skill) {
+                    Color::insert([
+                        [
+                            'skill_level'   => $i + 1,
+                            'hexa_color'    => '#ffffff',
+                            'skill_id'      => $skill->id
+                        ],[
+                            'skill_level'   => $i + 1,
+                            'hexa_color'    => '#fce844',
+                            'skill_id'      => $skill->id
+                        ],[
+                            'skill_level'   => $i + 1,
+                            'hexa_color'    => '#fea745',
+                            'skill_id'      => $skill->id
+                        ],[
+                            'skill_level'   => $i + 1,
+                            'hexa_color'    => '#318006',
+                            'skill_id'      => $skill->id
+                        ],[
+                            'skill_level'   => $i + 1,
+                            'hexa_color'    => '#fb4244',
+                            'skill_id'      => $skill->id
+                        ],[
+                            'skill_level'   => $i + 1,
+                            'hexa_color'    => '#347af8',
+                            'skill_id'      => $skill->id
+                        ]
+                    ]);
+                }
+
+                /* UPDATES
+                -------------------------- */
+                // foreach();
         });
     }
 }
